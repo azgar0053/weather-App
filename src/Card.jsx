f7cf28a96d69e21a0b5079612801fb8fa725ccb9
azgar0053 <azgar53ali@gmail.com>
2024-01-07 16:13:04 +0530

    added card class

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = (props) => {
   return (
-    <div style={{display:'flex', flexDirection:'column',justifyContent:'center',alignContent:'space-evenly', borderRadius:'5px', width:'150px', height:'auto',backgroundColor:'white', boxShadow:'2px 2px 0px #eee' }}>
+    <div className='weather-card' style={{display:'flex', flexDirection:'column',justifyContent:'center',alignContent:'space-evenly', borderRadius:'5px', width:'150px', height:'auto',backgroundColor:'white', boxShadow:'2px 2px 0px #eee' }}>
         <h3>{props.title}</h3>
         <p>{props.value}</p>
     </div>
